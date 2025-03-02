eda785b4340344cbc67c266dca88c07972cd6c24
seg <segnities007@gmail.com>
2025-03-02 14:34:48 +0900

    Set: Set some dependency related to Supabase.

--- composeApp/src/commonMain/kotlin/org/segnities007/blog/App.kt
+++ composeApp/src/commonMain/kotlin/org/segnities007/blog/App.kt
@@ -2,12 +2,16 @@ package org.segnities007.blog
 
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.*
+import kotlinx.datetime.Clock
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.segnities007.blog.ui.navigation.Navigation
 
 @Composable
 @Preview
 fun App() {
+
     MaterialTheme {
         Navigation()
     }
---
+++ composeApp/src/commonMain/kotlin/org/segnities007/blog/data/model/Page.kt
@@ -0,0 +1,20 @@
+package org.segnities007.blog.data.model
+
+import kotlinx.datetime.Clock
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import kotlinx.serialization.Serializable
+
+data class Page(
+    @Serializable
+    val pageID: Int = 0,
+    @Serializable
+    val title: String = "",
+    @Serializable
+    val description: String = "",
+    @Serializable
+    val createAt: LocalDate = Clock.System.now().toLocalDateTime(TimeZone.UTC).date,
+    @Serializable
+    val updateAt: LocalDate = Clock.System.now().toLocalDateTime(TimeZone.UTC).date,
+)
